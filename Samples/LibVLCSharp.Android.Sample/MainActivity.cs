6d65254cd53c21b4cbbf841bffa525bffee36c03
Martin Finkel <martin@videolabs.io>
2019-06-27 14:05:04 +0700

    native crash repro

--- Samples/LibVLCSharp.Android.Sample/MainActivity.cs
+++ Samples/LibVLCSharp.Android.Sample/MainActivity.cs
@@ -1,24 +1,21 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Views;
-using Android.Widget;
 using LibVLCSharp.Shared;
-using VideoView = LibVLCSharp.Platforms.Android.VideoView;
+using System;
+using System.Collections.Generic;
 
 namespace LibVLCSharp.Android.Sample
 {
     [Activity(Label = "LibVLCSharp.Android.Sample", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        VideoView _videoView;
         LibVLC _libVLC;
-        MediaPlayer _mediaPlayer;
+        List<MediaDiscoverer> _mediaDiscoverers = new List<MediaDiscoverer>();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);            
         }
 
@@ -28,24 +25,22 @@ namespace LibVLCSharp.Android.Sample
 
             Core.Initialize();
 
-            _libVLC = new LibVLC();
-            _mediaPlayer = new MediaPlayer(_libVLC)
-            {
-                EnableHardwareDecoding = true
-            };
+            _libVLC = new LibVLC("--verbose=2");
 
-            _videoView = new VideoView(this) { MediaPlayer = _mediaPlayer };
-            AddContentView(_videoView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
-            var media = new Media(_libVLC, "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4", FromType.FromLocation);
-            _videoView.MediaPlayer.Play(media);
+            foreach (var md in _libVLC.MediaDiscoverers(MediaDiscovererCategory.Lan))
+            {
+                var discoverer = new MediaDiscoverer(_libVLC, md.Name);
+                discoverer.MediaList.ItemAdded += MediaList_ItemAdded;
+                _mediaDiscoverers.Add(discoverer);
+            }
+
+            foreach (var md in _mediaDiscoverers)
+                md.Start();
         }
 
-        protected override void OnPause()
+        private void MediaList_ItemAdded(object sender, MediaListItemAddedEventArgs e)
         {
-            base.OnPause();
-
-            _videoView.MediaPlayer.Stop();
-            _videoView.Dispose();
+            System.Diagnostics.Debug.WriteLine("FOUND --------------------------> " + e.Media);
         }
     }
 }
