da97c430f5bbd14b92b9c9e8cb203c49bd594016
Martin Finkel <finkel2804@gmail.com>
2020-03-17 17:35:08 +0100

    initial libvlcsharp/monotorrent integration

--- Samples/LibVLCSharp.NetCore.Sample/Program.cs
+++ Samples/LibVLCSharp.NetCore.Sample/Program.cs
@@ -1,24 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using LibVLCSharp.Shared;
+using MonoTorrent;
+using MonoTorrent.Client;
+using MonoTorrent.Streaming;
 
 namespace LibVLCSharp.NetCore.Sample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var torrentPath = Path.Combine(Environment.CurrentDirectory, "video.torrent");
+            var downloadPath = Path.Combine(Environment.CurrentDirectory, "download");
+
+            const string torrentLink = "https://zoink.ch/torrent/Better.Call.Saul.S05E05.480p.x264-mSD[eztv].mkv.torrent";
+
+            var engine = new ClientEngine();
+
+            var torrent = Torrent.Load(new Uri(torrentLink), torrentPath);
+
+            var provider = new StreamProvider(engine, downloadPath, torrent);
+
+            await provider.StartAsync();
+
+            var stream = await provider.CreateStreamAsync(provider.Manager.Torrent.Files[0]);
+
             Core.Initialize();
 
-            using(var libVLC = new LibVLC())
-            {
-                var media = new Media(libVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4", FromType.FromLocation);
-                using (var mp = new MediaPlayer(media))
-                {
-                    media.Dispose();
-                    mp.Play();
-                    Console.ReadKey();
-                }
-            }
+            using var libVLC = new LibVLC();
+            using var media = new Media(libVLC, new StreamMediaInput(stream));
+            using var mp = new MediaPlayer(media);
+
+            mp.Play();
+
+            Console.ReadKey();
         }
     }
 }
