0891d5a33063b9fa2f43f0fa329b0cc2bcc37b5b
Martin Finkel <finkel2804@gmail.com>
2020-04-01 20:09:59 +0200

    Add logo watermark

--- Samples/LibVLCSharp.NetCore.Sample/Program.cs
+++ Samples/LibVLCSharp.NetCore.Sample/Program.cs
@@ -12,6 +12,8 @@ namespace LibVLCSharp.NetCore.Sample
             using var libVLC = new LibVLC();
             using var media = new Media(libVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4", FromType.FromLocation);
             using var mp = new MediaPlayer(media);
+            mp.SetLogoString(VideoLogoOption.File, "logo.png");
+            mp.SetLogoInt(VideoLogoOption.Enable, 1);
             mp.Play();
             Console.ReadKey();
         }
