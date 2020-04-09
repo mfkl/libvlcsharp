91e298045eef4b877bebe87f11437f258e5d4e35
Martin Finkel <finkel2804@gmail.com>
2020-04-09 12:38:37 +0200

    repro

---
+++ samples/WindowsFormsControlLibrary1/UserControl1.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            Core.Initialize();
+        }
+    }
+}
