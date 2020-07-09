a24880e9b9d6d9019018f26c8d82633270d8ebe5
Martin Finkel <finkel2804@gmail.com>
2020-07-09 11:14:35 +0200

    add repro

--- samples/LibVLCSharp.NetCore.Sample/Program.cs
+++ samples/LibVLCSharp.NetCore.Sample/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using LibVLCSharp.Shared;
 
 namespace LibVLCSharp.NetCore.Sample
@@ -9,8 +12,21 @@ namespace LibVLCSharp.NetCore.Sample
         {
             Core.Initialize();
 
-            using var libVLC = new LibVLC();
-            using var media = new Media(libVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"));
+            using var libVLC = new LibVLC("-vv");
+            libVLC.Log += (s, e) => System.Diagnostics.Debug.WriteLine(e.FormattedLog);
+            var FileStream = new FileStream(path: @"C:\Users\Martin\Downloads\video.ded", mode: FileMode.Open, access: FileAccess.Read);
+            RijndaelManaged AES = new RijndaelManaged();
+            SHA256 SHA256 = SHA256.Create();
+
+            // read key from TextBox4.Text
+            AES.Key = SHA256.ComputeHash(Encoding.ASCII.GetBytes("U[#x5:jg0$e-^etBx#MjWH5Zu_ndd9"));
+            AES.Mode = CipherMode.ECB;
+
+
+            // cryptostream starts here
+            var cryptoStream = new CryptoStream(FileStream, AES.CreateDecryptor(), CryptoStreamMode.Read);
+            using var input = new StreamMediaInput(cryptoStream);
+            using var media = new Media(libVLC, input);
             using var mp = new MediaPlayer(media);
             mp.Play();
             Console.ReadKey();
