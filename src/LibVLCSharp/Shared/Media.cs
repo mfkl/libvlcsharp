2226253ed2fbcffb6b6c1ac15ee681f1ea5d1255
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2020-11-12 22:16:35 +0100

    Fixed Nullable reference types-related warnings

--- src/LibVLCSharp/Shared/Media.cs
+++ src/LibVLCSharp/Shared/Media.cs
@@ -456,7 +456,7 @@ namespace LibVLCSharp.Shared
                 tcs.TrySetCanceled();
             });
 
-            void OnParsedChanged(object sender, MediaParsedChangedEventArgs mediaParsedChangedEventArgs) 
+            void OnParsedChanged(object? sender, MediaParsedChangedEventArgs mediaParsedChangedEventArgs) 
                 => tcs.TrySetResult(mediaParsedChangedEventArgs.ParsedStatus);
 
             try
@@ -583,7 +583,7 @@ namespace LibVLCSharp.Shared
         /// </summary>
         /// <param name="obj">the media to compare this one with</param>
         /// <returns></returns>
-        public override bool Equals(object obj)
+        public override bool Equals(object? obj)
         {
             return obj is Media media &&
                    EqualityComparer<IntPtr>.Default.Equals(NativeReference, media.NativeReference);
