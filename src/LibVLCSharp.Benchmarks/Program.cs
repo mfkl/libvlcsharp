1099961d9f1aa79590ee67419f96d15f4c2873ce
Martin Finkel <martin@videolabs.io>
2021-03-17 15:03:18 +0700

    initial benchmark setup

---
+++ src/LibVLCSharp.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace LibVLCSharp.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new BenchmarkSwitcher(typeof(Program).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+}
