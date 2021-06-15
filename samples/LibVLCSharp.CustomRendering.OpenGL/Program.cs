49a95bf4f85fcd6e5ba0ac38da00b84fe5cb6b45
Martin Finkel <me@martinfinkel.com>
2021-06-15 14:14:48 +0700

    opengl sample: create initial project

---
+++ samples/LibVLCSharp.CustomRendering.OpenGL/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Windowing.Desktop;
+using System;
+
+namespace LibVLCSharp.CustomRendering.OpenGL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Game game = new Game(new GameWindowSettings(), new NativeWindowSettings() { Title = "LibVLCSharp OpenGL sample" });
+            game.Run();
+        }
+    }
+
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+    }
+}
