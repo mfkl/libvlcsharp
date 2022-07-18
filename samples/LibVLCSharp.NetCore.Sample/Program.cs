5e654cb60875111cc85cd37cc4d3b2b6ccfd9ccb
Martin Finkel <me@martinfinkel.com>
2022-07-18 19:27:19 +0700

    repro live555 multi stream

--- samples/LibVLCSharp.NetCore.Sample/Program.cs
+++ samples/LibVLCSharp.NetCore.Sample/Program.cs
@@ -8,9 +8,15 @@ namespace LibVLCSharp.NetCore.Sample
         static void Main(string[] args)
         {
             using var libVLC = new LibVLC(enableDebugLogs: true);
-            using var media = new Media(new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"));
-            using var mp = new MediaPlayer(libVLC, media);
-            mp.Play();
+            using var media = new Media(new Uri("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"));
+            using var mp1 = new MediaPlayer(libVLC, media);
+            using var mp2 = new MediaPlayer(libVLC, media);
+            using var mp3 = new MediaPlayer(libVLC, media);
+            using var mp4 = new MediaPlayer(libVLC, media);
+            mp1.Play();
+            mp2.Play();
+            mp3.Play();
+            mp4.Play();
             Console.ReadKey();
         }
     }
