e5579b8335ee163d3c8c9a20634b32ff15652ab2
John Knipper <code@jkn.me>
2024-09-28 23:17:54 +0200

    Ported MAUI component changes to 3.x branch

---
+++ samples/MAUI/LibVLCSharp.MAUI.Sample.MediaElement/MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LibVLCSharp.MAUI.Sample.MediaElement
+{
+    /// <summary>
+    /// The MauiProgram class is responsible for creating and configuring the MAUI application.
+    /// </summary>
+    public static class MauiProgram
+    {
+        /// <summary>
+        /// Creates and configures the MAUI application.
+        /// </summary>
+        /// <returns>The configured MAUI application.</returns>
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureMauiHandlers(handlers =>
+                {
+                    // Adds a custom handler for the VideoView control.
+                    handlers.AddHandler<VideoView, VideoViewHandler>();
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
