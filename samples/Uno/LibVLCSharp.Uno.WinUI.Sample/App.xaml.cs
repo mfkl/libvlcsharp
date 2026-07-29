e43c08c5a6820e466d4a5607d02faba8dd891ce7
joshua.sells <joshua.sells@gmail.com>
2026-07-29 14:24:06 -0500

    UNO-Sample comment changes

--- samples/Uno/LibVLCSharp.Uno.WinUI.Sample/App.xaml.cs
+++ samples/Uno/LibVLCSharp.Uno.WinUI.Sample/App.xaml.cs
@@ -15,9 +15,9 @@ public partial class App : Application
     public App()
     {
         this.InitializeComponent();
-#if HAS_UNO
-        // Suspending/Resuming are UWP/Uno lifecycle events provided by Uno.WinUI on the mobile heads.
-        // WinUI 3 (native Windows head) removed them, so guard this wiring to the Uno targets.
+#if !WINDOWS
+        // Suspending/Resuming are Uno lifecycle events (cloned from UWP) and provided by Uno.UI on the mobile heads.
+        // WinUI 3 removed them, so guard this wiring to the Uno-mobile targets.
         Suspending += OnSuspending;
         Resuming += OnResuming;
 #endif
@@ -131,7 +131,7 @@ public partial class App : Application
 
         global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory = factory;
 
-#if HAS_UNO
+#if !WINDOWS
         global::Uno.UI.Adapter.Microsoft.Extensions.Logging.LoggingAdapter.Initialize();
 #endif
 #endif
@@ -144,7 +144,7 @@ public partial class App : Application
     /// </summary>
     /// <param name="sender">The source of the suspend request.</param>
     /// <param name="e">Details about the suspend request.</param>
-#if HAS_UNO
+#if !WINDOWS
     private void OnSuspending(object sender, SuspendingEventArgs e)
     {
         var deferral = e.SuspendingOperation.GetDeferral();
